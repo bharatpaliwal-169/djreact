80d3edec2756ee53888eeb00f8f7c8bdb77e1fa6
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-03-20 13:12:27 +0530

    CRUD ViewSet op

--- article/api/views.py
+++ article/api/views.py
@@ -1,21 +1,51 @@
+from rest_framework import viewsets
+
 from rest_framework.generics import (
     ListAPIView,
-    RetrieveAPIView
+    RetrieveAPIView,
+    CreateAPIView,
+    DestroyAPIView,
+    UpdateAPIView
 )
-
-
-from article.models import Article
-
+from ..models import Article
 from .serializers import ArticleSerializer
 
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-    # permission_classes = (permissions.AllowAny, )
+
+#CRUD
+
+# #READ_ONLY
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#     # permission_classes = (permissions.AllowAny, )
+
+# #GET _ONLY
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#     # permission_classes = (permissions.AllowAny, )
+
+# #POST
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+# class ArticleUpdateView(UpdateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#     # permission_classes = (permissions.IsAuthenticated, )
 
 
-class ArticleDetailView(RetrieveAPIView):
+# class ArticleDeleteView(DestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#     # permission_classes = (permissions.IsAuthenticated, )
+
+
+
+# this viewset is enough to handle all basic CRUD operations.
+#however we will use the generic views as we need more customization for auth and other features.
+class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    # permission_classes = (permissions.AllowAny, )
